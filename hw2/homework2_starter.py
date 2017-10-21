461a07de7cccf619f4da762176008e4c305501dd
Inyoung Woo <inwoo@ucsd.edu>
2017-10-21 15:40:37 -0700

    q1 done

--- hw2/homework2_starter.py
+++ hw2/homework2_starter.py
@@ -1,5 +1,4 @@
 import numpy
-from urllib.request import urlopen
 import scipy.optimize
 import random
 from math import exp
@@ -9,16 +8,18 @@ def parseData(fname):
   for l in open(fname):
     yield eval(l)
 
-print "Reading data..."
-data = list(parseData("beer_50000.json"))
-print "done"
 
 def feature(datum):
   feat = [1, datum['review/taste'], datum['review/appearance'], datum['review/aroma'], datum['review/palate'], datum['review/overall']]
   return feat
 
-X = [feature(d) for d in data]
-y = [d['beer/ABV'] >= 6.5 for d in data]
+def prepData():
+    print "Reading data..."
+    data = list(parseData("beer_50000.json"))
+    print "done"
+    X = [feature(d) for d in data]
+    y = [d['beer/ABV'] >= 6.5 for d in data]
+    return X, y
 
 def inner(x,y):
   return sum([x[i]*y[i] for i in range(len(x))])
@@ -57,34 +58,36 @@ def fprime(theta, X, y, lam):
     dl[k] -= lam*2*theta[k]
   return numpy.array([-x for x in dl])
 
-X_train = X
-y_train = y
-
 ##################################################
 # Train                                          #
 ##################################################
 
-def train(lam):
-  theta,_,_ = scipy.optimize.fmin_l_bfgs_b(f, [0]*len(X[0]), fprime, pgtol = 10, args = (X_train, y_train, lam))
+def train(lam, X, y):
+  theta,_,_ = scipy.optimize.fmin_l_bfgs_b(f, [0]*len(X[0]), fprime, pgtol = 10, args = (X, y, lam))
   return theta
 
 ##################################################
 # Predict                                        #
 ##################################################
 
-def performance(theta):
+def performance(theta, X, y):
   scores = [inner(theta,x) for x in X]
   predictions = [s > 0 for s in scores]
-  correct = [(a==b) for (a,b) in zip(predictions,y_train)]
+  correct = [(a==b) for (a,b) in zip(predictions,y)]
   acc = sum(correct) * 1.0 / len(correct)
   return acc
 
 ##################################################
 # Validation pipeline                            #
 ##################################################
+def validate(X, y):
+    lam = 1.0
+    theta = train(lam, X, y)
+    acc = performance(theta, X, y)
+    print("lambda = " + str(lam) + ":\taccuracy=" + str(acc))
 
-lam = 1.0
+def test():
+    X, y = prepData()
+    validate(X, y)
 
-theta = train(lam)
-acc = performance(theta)
-print("lambda = " + str(lam) + ":\taccuracy=" + str(acc))
+#test()
